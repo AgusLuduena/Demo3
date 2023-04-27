1473af5df077e51550ae8838ec073e62101f37b4
AgusLuduena <127694554+AgusLuduena@users.noreply.github.com>
2023-04-27 15:55:41 -0300

    Add files via upload

---
+++ Ejercicio3/Ejercicio3.java
@@ -0,0 +1,18 @@
+package Ejercicio3;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+
+        String[] nombre = {"Agustin", "Adriana", "Nicolas", "Oscar"};
+        System.out.println("ingrese un valor:");
+
+
+        for (int i = 0; i < nombre.length; i++) {
+            System.out.println(nombre[i]);
+        }
+
+    }
+}
+
+
